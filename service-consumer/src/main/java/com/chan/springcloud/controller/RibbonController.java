13b74eb7f57874d73e5ffa122def2f72c6fddbd9
jingshanccc <jingshanccc@qq.com>
2020-07-26 15:29:20 +0800

    1. 客户端整合Hystrix

--- service-consumer/src/main/java/com/chan/springcloud/controller/RibbonController.java
+++ service-consumer/src/main/java/com/chan/springcloud/controller/RibbonController.java
@@ -1,5 +1,6 @@
 package com.chan.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,13 @@ public class RibbonController {
     private RestTemplate template;
 
     @GetMapping("/hello")
+    @HystrixCommand(fallbackMethod = "hystrixFallback")
     public String hello(){
-        return template.getForObject(PREFIX+"hello",String.class, (Object) null);
+        return template.getForObject(PREFIX+"hello",String.class,(Object)null);
+    }
+
+    public String hystrixFallback(){
+        return "hystrix fallback hello";
     }
 
 }
